d363747ad1d6a2948c129f85109585c1402f07bd
Pedro Rodrigues <58788062+PedroRBC@users.noreply.github.com>
2023-04-02 17:29:55 -0300

    :package: | Installed Gesture, Animated Handler's
    Signed-off-by: Pedro Rodrigues <58788062+PedroRBC@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/routes';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { SQLiteProvider } from './src/contexts/SQlite';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,9 +20,11 @@ export default function App() {
   }
 
   return (
-    <SQLiteProvider>
-      <Routes />
+    <GestureHandlerRootView style={{ flex: 1 }} >
+      <SQLiteProvider>
+        <Routes />
+      </SQLiteProvider>
       <StatusBar style="auto" translucent backgroundColor='transparent' />
-    </SQLiteProvider>
+    </GestureHandlerRootView>
   );
 }
